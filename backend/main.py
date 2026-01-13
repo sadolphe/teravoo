81cb9937ee8c0ed0a599bb66e5adc2dc40dadcc2
Steeve ADOLPHE <steeveadolphe@Steeves-MacBook-Pro-2.local>
2026-01-13 23:44:24 +0100

    fix(backend): allow CORS for all Vercel preview URLs

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,7 @@ if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origin_regex=r"^https://teravoo-core.*\.vercel\.app$",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],

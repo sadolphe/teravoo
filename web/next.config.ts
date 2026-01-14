412e3cf0d32ee230ff4a33c405fe3a2572a7f1ee
Steeve ADOLPHE <adolphe.sa@gmail.com>
2026-01-14 19:17:06 +0100

    fix(web): use trailingSlash true and remove manual redirect to fix loop

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,20 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  trailingSlash: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     ignoreBuildErrors: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/mobile',
-        destination: '/mobile/',
-        permanent: true,
-      }
-    ];
   },
   async rewrites() {
     return {
